cb6514a7df8a1cc2219b22c74963c5793d66f2f2
n6rdk <nabilaoktavia1975@gmail.com>
2026-09-02 15:35:23 +0700

    .

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -90,6 +90,7 @@ if PRODUCTION:
             'HOST': os.getenv('DB_HOST'),
             'PORT': os.getenv('DB_PORT'),
             'OPTIONS': {
+                
                 'options': f"-c search_path={os.getenv('SCHEMA', 'public')}"
             }
         }
