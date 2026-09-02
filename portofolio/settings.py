eb2c8ede267fec8cd93b63ac503b0684fcb970e6
n6rdk <nabilaoktavia1975@gmail.com>
2026-09-02 15:32:35 +0700

    fix: fix another error for pws deploy

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -142,6 +142,7 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 WHITENOISE_USE_FINDERS = True
 
+
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
 
