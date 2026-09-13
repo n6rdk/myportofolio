67d56f9a6e0a499d05c5271714f159e862287642
n6rdk <nabilaoktavia1975@gmail.com>
2026-09-13 15:39:50 +0700

    feat: redesign experience page, add some animations, and switch to local Tailwind build

--- main/models.py
+++ main/models.py
@@ -9,6 +9,7 @@ class Experience(models.Model):
         ('part-time', 'Part-Time'),
         ('full-time', 'Full-Time'),
         ('freelance', 'Freelance'),
+        ('competition', 'Competition'),
     ]
     
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -16,11 +17,15 @@ class Experience(models.Model):
     description = models.TextField()
     category = models.CharField(max_length=20, choices=EXPERIENCE_CHOICES, default='full-time')
     thumbnail = models.URLField(blank=True, null=True)
-    started_at = models.DateTimeField(auto_now_add=True)
-    ended_at = models.DateTimeField(blank=True, null=True)
+    started_at = models.PositiveIntegerField(blank=True, null=True)
+    ended_at = models.PositiveIntegerField(blank=True, null=True)
     def __str__(self):
         return self.title
     
     @property
     def is_ongoing(self):
         return self.ended_at is None
+    
+    @property
+    def is_blank_start(self):
+        return self.started_at is None
