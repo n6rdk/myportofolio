8452a76f6a099a77418cf31d21fa453644ad638b
n6rdk <nabilaoktavia1975@gmail.com>
2026-09-13 15:49:56 +0700

    fix: add missing variable in views.py

--- main/views.py
+++ main/views.py
@@ -5,9 +5,11 @@ from main.models import Experience
 
 def show_main(request):
     context = {
-        "name": "Nabila",
+        "first_name": "Nabila",
+        "middle_name": "Oktavia",
+        "last_name": "Ramadhani",
         "npm": "2506593613",
-        "study_program": "S1 Ilmu Komputer",
+        "major": "S1 Ilmu Komputer",
         "bio": (
             "CS student at Universitas Indonesia, currently exploring my interests in data science and cybersecurity. I'm a quiet thinker who prefers observing, analyzing, and solving problems behind the scenes."
         ),
@@ -17,7 +19,9 @@ def show_main(request):
 
 def show_experience(request):
     context = {
-        "name": "Nabila",
+        "first_name": "Nabila",
+        "middle_name": "Oktavia",
+        "last_name": "Ramadhani",
         "experience_list": Experience.objects.all(),
     }
     return render(request, "experience.html", context)
