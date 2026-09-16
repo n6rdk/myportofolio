b05932523fea4aa8fd440d9a9e0c786266a9f94a
n6rdk <nabilaoktavia1975@gmail.com>
2026-09-16 22:27:11 +0700

    fix: change Django secret key environment variable

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 PORTFOLIO_SECRET_KEY = os.getenv("PORTFOLIO_SECRET_KEY")
 
